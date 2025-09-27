9a19b09c936cb3acbb14ee554030e23d92478b9c
cmcarpen86 <chasecarpenter84@gmail.com>
2025-09-27 11:40:24 -0400

    New Shooter Code (Shootaaa)

--- TeamCode/src/main/java/Modules/OCBHWM.java
+++ TeamCode/src/main/java/Modules/OCBHWM.java
@@ -1,5 +1,7 @@
 package Modules;
 
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorGroup;
 import com.qualcomm.hardware.rev.Rev9AxisImu;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -10,30 +12,40 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class OCBHWM {
 
     public static Rev9AxisImu imu;
-    public static DcMotorEx leftFront;
-    public static DcMotorEx rightFront;
-    public static DcMotorEx rightBack;
-    public static DcMotorEx leftBack;
-    public static DcMotorEx shootaL;
-    public static DcMotorEx shootaR;
-    public static DcMotorEx transferM;
-    public static DcMotorEx intakeM;
+    public static Motor leftFront;
+    public static Motor rightFront;
+    public static Motor rightBack;
+    public static Motor leftBack;
+    public static Motor shootaL;
+    public static Motor shootaR;
+    public static Motor transferM;
+    public static Motor intakeM;
+    public static MotorGroup flyWheel;
     public HardwareMap hardwareMap;
+
+    public static double kP = 20;
+    public static double kV = 0.7;
 
     public static void hwinit(HardwareMap hardwareMap) {
 
         imu = hardwareMap.get(Rev9AxisImu.class, "imu");
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        leftFront = hardwareMap.get(Motor.class, "leftFront");
+        rightFront = hardwareMap.get(Motor.class, "rightFront");
+        leftBack = hardwareMap.get(Motor.class, "leftBack");
+        rightBack = hardwareMap.get(Motor.class, "rightBack");
 
-        shootaL = hardwareMap.get(DcMotorEx.class, "shootaL");
-        shootaR = hardwareMap.get(DcMotorEx.class, "shootaR");
+        flyWheel = new MotorGroup(
+                new Motor(hardwareMap, "shootaL", Motor.GoBILDA.BARE),
+                new Motor(hardwareMap, "shootaL", Motor.GoBILDA.BARE)
+        );
 
-        transferM = hardwareMap.get(DcMotorEx.class, "transferM");
+        flyWheel.setRunMode(Motor.RunMode.VelocityControl);
+        flyWheel.setVeloCoefficients(kP, 0, 0);
+        flyWheel.setFeedforwardCoefficients(0, kV);
 
-        intakeM = hardwareMap.get(DcMotorEx.class, "transferM");
+        transferM = hardwareMap.get(Motor.class, "transferM");
+
+        intakeM = hardwareMap.get(Motor.class, "transferM");
 
     }
 }
