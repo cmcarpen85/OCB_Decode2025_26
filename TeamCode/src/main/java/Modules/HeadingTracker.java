d62bb2e0eadee41933c9d5381545fc3bb89005aa
cmcarpen86 <chasecarpenter84@gmail.com>
2026-03-05 17:59:13 -0500

    Gyros Working

--- TeamCode/src/main/java/Modules/HeadingTracker.java
+++ TeamCode/src/main/java/Modules/HeadingTracker.java
@@ -8,6 +8,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.opencv.core.Mat;
 
+import java.util.function.ToDoubleBiFunction;
+
 public class HeadingTracker {
     public double robotOri;
     public double turretOri;
@@ -23,18 +25,55 @@ public class HeadingTracker {
         robotY = startY;
         robotOri = startOri;
     }
-
-    public static void drivetrainDistanceBlue() {
+// TODO add * -1 to one of the two
+    public static double headingTrackingBlue() {
         Pose2D currentPos = OCBHWM.pinPoint.getPosition();
         double XDistance = Math.abs(blueGoalX - currentPos.getX(DistanceUnit.INCH));
         double YDistance = Math.abs(blueGoalY - currentPos.getY(DistanceUnit.INCH));
         double robotDistance = Math.sqrt(Math.pow(XDistance,2) + Math.pow(YDistance,2));
-        double angleToGoal = Math.asin(YDistance/robotDistance);
-//        currentPos.getHeading() = angleToGoal;
+        double angleToGoal = Math.asin(YDistance/robotDistance) * 180 / Math.PI;
+        return headingDifference(angleToGoal);
+    }
+
+    public static double headingTrackingRed() {
+        Pose2D currentPos = OCBHWM.pinPoint.getPosition();
+        double XDistance = Math.abs(redGoalX - currentPos.getX(DistanceUnit.INCH));
+        double YDistance = Math.abs(redGoalY - currentPos.getY(DistanceUnit.INCH));
+        double robotDistance = Math.sqrt(Math.pow(XDistance,2) + Math.pow(YDistance,2));
+        double angleToGoal = Math.asin(YDistance/robotDistance) * 180 / Math.PI;
+        return headingDifference(angleToGoal);
     }
 
     public static double gyroDifference() {
-       return OCBHWM.pinPoint.getHeading(AngleUnit.DEGREES)-OCBHWM.imu.getRobotYawPitchRollAngles().getYaw();
+        double pinPointHeading = OCBHWM.pinPoint.getHeading(AngleUnit.DEGREES);
+        double turretHeading = OCBHWM.imu.getRobotYawPitchRollAngles().getYaw();
+       if (Math.abs(pinPointHeading) < 90 && Math.abs(turretHeading) < 90) {
+           return pinPointHeading - turretHeading;
+       } else if (Math.abs(pinPointHeading) >= 90) {
+           double opAngle = -1 * (180 - Math.abs(pinPointHeading) * Math.signum(pinPointHeading));
+           if ((180 - Math.abs(turretHeading)) * (Math.signum(turretHeading)) >= opAngle) {
+           return pinPointHeading - turretHeading;
+           } else {
+            return (180 - Math.abs(turretHeading)) * Math.signum(turretHeading) - (180 - Math.abs(pinPointHeading)) * Math.signum(pinPointHeading);
+           }
+       } else {
+           return (pinPointHeading - turretHeading);
+       }
     }
 
+    public static double headingDifference(double desiredHeading) {
+        double turretHeading = OCBHWM.imu.getRobotYawPitchRollAngles().getYaw();
+        if (Math.abs(desiredHeading) < 90 && Math.abs(turretHeading) < 90) {
+            return desiredHeading - turretHeading;
+        } else if (Math.abs(desiredHeading) >= 90) {
+            double opAngle = -1 * (180 - Math.abs(desiredHeading) * Math.signum(desiredHeading));
+            if ((180 - Math.abs(turretHeading)) * (Math.signum(turretHeading)) >= opAngle) {
+                return desiredHeading - turretHeading;
+            } else {
+                return (180 - Math.abs(turretHeading)) * Math.signum(turretHeading) - (180 - Math.abs(desiredHeading)) * Math.signum(desiredHeading);
+            }
+        } else {
+            return (desiredHeading - turretHeading);
+        }
+    }
 }
